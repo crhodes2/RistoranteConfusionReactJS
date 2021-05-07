22fca406eb717cc240f976f6f780b9af4b8696d0
Christian Rhodes <rhodeschristian@icloud.com>
2021-05-06 20:44:13 -0700

    react components part 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
 import './App.css';
+import Menu from './components/MenuComponent';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">
+            <h4 className="navi"> <RestaurantIcon/> Ristorante Confusion</h4>
+          </NavbarBrand>
+          <Menu />
+        </div>
+      </Navbar>
     </div>
   );
 }
